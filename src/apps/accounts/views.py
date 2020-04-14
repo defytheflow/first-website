deb0dfa57a10a8aa03cc6eefec4df63e916163eb
Artyom Danilov <defytheflow@gmail.com>
2020-04-14 15:06:03 +0300

    Add a login form to index page
    Type: feat

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,11 +1,29 @@
+from django.conf import settings
 from django.contrib.auth import login, logout
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 from django.views import View
-from django.conf import settings
 
 from .decorators import anonymous_required
 from .forms import LoginForm, SignupForm
+
+
+@method_decorator(anonymous_required, name='dispatch')
+class IndexView(View):
+
+    template_name = 'accounts/index.html'
+    form_class = LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return redirect(settings.LOGIN_URL)
 
 
 @method_decorator(anonymous_required, name='dispatch')
--- src/root/urls.py
+++ src/root/urls.py
@@ -3,16 +3,11 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 
-from accounts.decorators import anonymous_required
+from accounts.views import IndexView
 
 
 urlpatterns = [
-    path('',
-        anonymous_required(
-            TemplateView.as_view(template_name='index.html')
-        ),
-        name='index',
-    ),
+    path('', IndexView.as_view(), name='index'),
     path('home',
         login_required(
             TemplateView.as_view(template_name='home.html'),
